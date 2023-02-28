07fc7577a5c025d9f16d56ed7ada74927c39c7a2
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 13:34:10 -0600

    Add: Fluent Validation Dependencys :heavy_plus_sign:

--- backend/MemberActivities/Program.cs
+++ backend/MemberActivities/Program.cs
@@ -1,3 +1,4 @@
+using FluentValidation.AspNetCore;
 using MemberActivites.Infrastructure.Database;
 using MemberActivites.Infrastructure.Extension;
 using MemberActivities.Application.Service;
@@ -9,7 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(
+    options =>
+    {
+    // Validate child properties and root collection elements
+    options.ImplicitlyValidateChildProperties = true;
+    options.ImplicitlyValidateRootCollectionElements = true;
+
+    // Automatic registration of validators in assembly
+    options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
