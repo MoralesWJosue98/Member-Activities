cc92508f6ff73fd77d66617f8d86d3eb60ef6000
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 12:10:47 -0600

    Add: Unit Of Work extension :necktie:

---
+++ backend/MemberActivites.Infrastructure/Extension/UnitOfWorkExtension.cs
@@ -0,0 +1,33 @@
+﻿using MemberActivites.Infrastructure.Database;
+using MemberActivites.Infrastructure.Repositories;
+using MemberActivities.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Extension
+{
+    public static class UnitOfWorkExtension
+    {
+        public static IServiceCollection SetupUnitOfWork([NotNullAttribute] this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>(f =>
+            {
+                var scopeFactory = f.GetRequiredService<IServiceScopeFactory>();
+                var context = f.GetService<DatabaseContext>();
+
+                return new UnitOfWork(
+                    context,
+                    new MemberRepository(context.Member)
+                    );
+            });
+
+            return serviceCollection;
+
+        }
+    }
+}
