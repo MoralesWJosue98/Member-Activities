9de4c04ecdfb8013cc8880cb5ab7390a39362a80
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 12:21:29 -0600

    Add: Activity Entity, DTO and context :necktie:

---
+++ backend/MemberActivites.Infrastructure/Database/PermissionEntityActivityConfig.cs
@@ -0,0 +1,20 @@
+﻿using MemberActivities.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Database
+{
+    public class PermissionEntityActivityConfig : IEntityTypeConfiguration<Activity>
+    {
+        public void Configure(EntityTypeBuilder<Activity> builder)
+        {
+            builder.Property(p => p.NameActivity).IsRequired();
+            builder.Property(p => p.Description).IsRequired();
+        }
+    }
+}
---
+++ backend/MemberActivities.Domain/DTOs/ActivityDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivities.Domain.DTOs
+{
+    public class ActivityDTO
+    {
+        public int Id { get; set; }
+        public string NameActivity { get; set; }
+        public string Description { get; set; }
+    }
+}
