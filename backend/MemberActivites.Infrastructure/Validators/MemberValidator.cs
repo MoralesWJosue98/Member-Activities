007f0911d2fb19c5b73016b17feb53721e2e73f3
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 13:29:30 -0600

    Add: Member validator :necktie:

---
+++ backend/MemberActivites.Infrastructure/Validators/MemberValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MemberActivities.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Validators
+{
+    public class MemberValidator : AbstractValidator<MemberDTO>
+    {
+        public MemberValidator() {
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.LastName).NotNull();
+            RuleFor(x => x.email).NotNull();
+            RuleFor(x => x.Address).NotNull();
+           RuleFor(x => x.isMember).NotNull();
+
+        }
+
+    }
+}
