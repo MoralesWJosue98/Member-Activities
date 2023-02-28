6f6835aa0d9e20190f7f4e2e093dd9023c83312e
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 11:52:48 -0600

    Add: Mapper Profile in Repositories :necktie:

---
+++ backend/MemberActivites.Infrastructure/Mappers/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MemberActivities.Domain.DTOs;
+using MemberActivities.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Mappers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<MemberDTO, Member>().ReverseMap();
+        }
+    }
+}
