b0b9e4e82a6bff2a616b16cbc09a9c51a2511aae
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 10:50:13 -0600

    Add: Member Entities in Domain :necktie:

---
+++ backend/MemberActivities.Domain/Entities/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivities.Domain.Entities
+{
+    public class Member : BaseEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string email { get; set; }
+        public bool isMember { get; set; }  
+
+    }
+}
