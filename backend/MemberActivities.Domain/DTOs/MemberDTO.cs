048e92e979a6dbea6cccff7bfaa900d21585497c
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 10:53:24 -0600

    Add: Member DTO in Domain :necktie:

---
+++ backend/MemberActivities.Domain/DTOs/MemberDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivities.Domain.DTOs
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string email { get; set; }
+        public bool isMember { get; set; }  
+
+    }
+}
