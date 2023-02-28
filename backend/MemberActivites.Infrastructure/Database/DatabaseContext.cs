a27da5c64c9d83bc8b5015467949838e89f889d2
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-28 11:09:44 -0600

    Add: Database Context and config in Infrastructure :card_file_box:

---
+++ backend/MemberActivites.Infrastructure/Database/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using MemberActivities.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Member> Member { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PermissionEntityMemberConfig());
+        }
+
+
+    }
+}
---
+++ backend/MemberActivites.Infrastructure/Database/PermissionEntityMemberConfig.cs
@@ -0,0 +1,23 @@
+﻿using MemberActivities.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberActivites.Infrastructure.Database
+{
+    public class PermissionEntityMemberConfig : IEntityTypeConfiguration<Member>
+    {
+        public void Configure(EntityTypeBuilder<Member> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.LastName).IsRequired();
+            builder.Property(p => p.Address).IsRequired();
+            builder.Property(p => p.email).IsRequired();
+            builder.Property(p => p.isMember).IsRequired();
+        }
+    }
+}
